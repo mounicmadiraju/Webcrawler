119508e3c10f5045aa3df6567e876baf28c1ec2f
Mounic Madiraju <mounicraju@gmail.com>
2017-07-05 16:00:55 +0800

    Update AssemblyInfo.cs

--- ContentExtractor/AssemblyInfo.cs
+++ ContentExtractor/AssemblyInfo.cs
@@ -21,8 +21,6 @@ using System.Runtime.CompilerServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
-//      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
